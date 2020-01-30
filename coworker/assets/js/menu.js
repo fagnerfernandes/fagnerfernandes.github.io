4be076c970e515a517b5ee1f67b621c33d83ece5
Fagner Fernandes <fagner.ti@gmail.com>
2020-01-30 18:52:35 -0500

    landing page correcao de textos de whatssapp

--- coworker/assets/js/menu.js
+++ coworker/assets/js/menu.js
@@ -2,10 +2,26 @@ const menuDrop = document.querySelector('.menu');
 const btnMenuOpen = document.querySelector('.btn-menu-open');
 const btnMenuClose = document.querySelector('.btn-menu-close');
 
+const followPhotos = document.querySelector('a.followPhotos');
+const followLocation = document.querySelector('a.followLocation');
+const followPrice = document.querySelector('a.followPrice');
+
 btnMenuOpen.addEventListener('click', function(){
 	menuDrop.classList.add('menu-open');
 });
 
 btnMenuClose.addEventListener('click', function(){
+	menuDrop.classList.remove('menu-open');
+});
+
+followPhotos.addEventListener('click', function(){
+	menuDrop.classList.remove('menu-open');
+});
+
+followLocation.addEventListener('click', function(){
+	menuDrop.classList.remove('menu-open');
+});
+
+followPrice.addEventListener('click', function(){
 	menuDrop.classList.remove('menu-open');
 });
